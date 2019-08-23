d9ff61e33dc9267f5f608b721ae8273a229ae923
Kasia Kordys <kasia.kordys@gmail.com>
2019-08-23 22:28:04 +0100

    Add EF Core and create and initialize the database

--- Elephantico/Program.cs
+++ Elephantico/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Elephantico.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Elephantico
@@ -14,7 +16,31 @@ namespace Elephantico
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            // we need access to the dependency injection container available inMain method
+            // available through services collection
+            // that is how we get access to AppDbContext
+            // we need appDbContext to call Seed method on DbInitializer
+            // we call seed method here
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<AppDBContex>();
+                    DbInitializer.Seed(context);
+                }
+
+                catch (Exception)
+                {
+                    // nothing to log out for now
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
