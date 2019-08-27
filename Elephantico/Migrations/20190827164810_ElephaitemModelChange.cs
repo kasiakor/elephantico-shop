8c945495d3d3deb379f24add54730166ddd86bbc
Kasia Kordys <kasia.kordys@gmail.com>
2019-08-27 18:00:08 +0100

    Update database with a new property

---
+++ Elephantico/Migrations/20190827164810_ElephaitemModelChange.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Elephantico.Migrations
+{
+    public partial class ElephaitemModelChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsInStock",
+                table: "Elephaitems",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsInStock",
+                table: "Elephaitems");
+        }
+    }
+}
--- Elephantico/Models/Elephaitem.cs
+++ Elephantico/Models/Elephaitem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,10 +15,12 @@ namespace Elephantico.Models
         public string Name { get; set; }
         public string ShortDescription { get; set; }
         public string LongDescription { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
         public string ImageThumbnailUrl { get; set; }
         public bool IsElephaitemOfTheWeek { get; set; }
+        public bool IsInStock { get; set; }
 
     }
 }
